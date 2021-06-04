f462ee9af56fb987b5ec95bf8b547b38fe96aa49
wm <wm@manaosoftware.com>
2021-06-04 14:19:34 +0700

    Add onRequetClose

--- LightboxOverlay.js
+++ LightboxOverlay.js
@@ -217,13 +217,13 @@ export default class LightboxOverlay extends Component {
     }];
 
     const content = (
-      <Animated.View style={[openStyle, dragStyle]} useNativeDriver={true}>
+      <Animated.View style={[openStyle, dragStyle]} useNativeDriver={true} onRequestClose={() => this.close}>
         {this.props.children}
       </Animated.View>
     );
 
     const background = (
-      <Animated.View style={[styles.background, { backgroundColor: backgroundColor }, lightboxOpacityStyle]} onPress={this.close} useNativeDriver={true}>
+      <Animated.View style={[styles.background, { backgroundColor: backgroundColor }, lightboxOpacityStyle]} onRequestClose={() => this.close} useNativeDriver={true}>
       </Animated.View>);
 
     const header = (
