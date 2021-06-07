5d03e38d50e54ad89ea0ff5bb69d0193aa91479c
wm <wm@manaosoftware.com>
2021-06-07 11:47:06 +0700

    Update content

--- LightboxOverlay.js
+++ LightboxOverlay.js
@@ -217,7 +217,7 @@ export default class LightboxOverlay extends Component {
       height: openVal.interpolate({inputRange: [0, 1], outputRange: [origin.height, WINDOW_HEIGHT]}),
     }];
 
-    const background = (<Animated.View style={[styles.background, { backgroundColor: backgroundColor }, lightboxOpacityStyle]}  {...handlers}></Animated.View>);
+    const background = (<Animated.View style={[styles.background, { backgroundColor: backgroundColor }, lightboxOpacityStyle]}></Animated.View>);
     const header = (<Animated.View style={[styles.header, lightboxOpacityStyle]}>{(renderHeader ?
       renderHeader(this.close) :
       (
