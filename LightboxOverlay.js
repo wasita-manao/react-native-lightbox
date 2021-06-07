0a1f15f6c830114e3d41902d41610ca41bea6669
wm <wm@manaosoftware.com>
2021-06-07 16:38:40 +0700

    Update content style

--- LightboxOverlay.js
+++ LightboxOverlay.js
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
   },
   open: {
     position: 'absolute',
-    flex: 1,
+    // flex: 1,
     justifyContent: 'center',
     // Android pan handlers crash without this declaration:
     backgroundColor: 'red',
@@ -229,7 +229,7 @@ export default class LightboxOverlay extends Component {
       )
     )}</Animated.View>);
     const content = (
-      <Animated.View style={[styles.open, dragStyle], {width: WINDOW_WIDTH, height: WINDOW_HEIGHT}}>
+      <Animated.View style={[styles.open, dragStyle], {width: WINDOW_WIDTH, height: WINDOW_HEIGHT, backgroundColor: 'red'}}>
         {this.props.children}
       </Animated.View>
     );
