17d1884721eb765ed992b6d7d14ca337d171429b
wm <wm@manaosoftware.com>
2021-06-07 14:26:43 +0700

    Update content style

--- LightboxOverlay.js
+++ LightboxOverlay.js
@@ -20,6 +20,9 @@ const styles = StyleSheet.create({
     position: 'absolute',
     // flex: 1,
     justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
     // Android pan handlers crash without this declaration:
     backgroundColor: 'red',
   },
@@ -216,9 +219,6 @@ export default class LightboxOverlay extends Component {
       top:    openVal.interpolate({inputRange: [0, 1], outputRange: [origin.y + STATUS_BAR_OFFSET, target.y + STATUS_BAR_OFFSET]}),
       width:  openVal.interpolate({inputRange: [0, 1], outputRange: [origin.width, WINDOW_WIDTH]}),
       height: openVal.interpolate({inputRange: [0, 1], outputRange: [origin.height, WINDOW_HEIGHT-160]}),
-      // justifyContent: 'center',
-      // alignItem: 'center',
-      // alignSelf: 'center'
     }
   ];
 
