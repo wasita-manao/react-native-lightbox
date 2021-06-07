a10087a7e6ea89703bdb4c5180a771d6e61ac712
wm <wm@manaosoftware.com>
2021-06-07 12:46:41 +0700

    Update content height

--- LightboxOverlay.js
+++ LightboxOverlay.js
@@ -215,7 +215,7 @@ export default class LightboxOverlay extends Component {
       left:   openVal.interpolate({inputRange: [0, 1], outputRange: [origin.x, target.x]}),
       top:    openVal.interpolate({inputRange: [0, 1], outputRange: [origin.y + STATUS_BAR_OFFSET, target.y + STATUS_BAR_OFFSET]}),
       width:  openVal.interpolate({inputRange: [0, 1], outputRange: [origin.width, WINDOW_WIDTH]}),
-      height: openVal.interpolate({inputRange: [0, 1], outputRange: [origin.height, 1250]}),
+      height: openVal.interpolate({inputRange: [0, 1], outputRange: [origin.height, WINDOW_HEIGHT/0.5]}),
     }
   ];
 
